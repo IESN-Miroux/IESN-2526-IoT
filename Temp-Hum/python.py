2cd93946b1ef36bc3bfdc1f8b6860034b53c1dc0
JustinMiroux <justin@miroux.co>
2025-10-09 11:43:57 +0200

    add python part

---
+++ Temp-Hum/python.py
@@ -0,0 +1,12 @@
+import serial
+
+USB = "/dev/ttyACM0"
+SER = serial.Serial(USB, baudrate=9600, dsrdtr=False)
+
+while True:
+
+    ligne = SER.readline()
+    strlignes = ligne.decode("utf-8")
+    strligne = strlignes.strip()
+
+    print(strligne)
